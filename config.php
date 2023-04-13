936f6a961f25aeae7485757b466d2362dd0c2632
WuHuLing <121509135+wjy4399@users.noreply.github.com>
2023-04-13 12:12:34 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/*数据库配置*/
+$dbconfig=array(
+	"host" => "xinian.mysql.database.azure.com", //数据库服务器
+	"port" => 3306, //数据库端口
+	"user" => "xinian", //数据库用户名
+	"pwd" => "WJYwhl1306848650", //数据库密码
+	"dbname" => "xinianshuju", //数据库名
+	"dbqz" => "xinianshuju" //数据表前缀
+);
+?>
